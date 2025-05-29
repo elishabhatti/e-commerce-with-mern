69011d1d2acca7fc19833773e155f1dd90469100
elisha <elishajameel2007@gmail.com>
2025-05-29 04:28:13 -0700

    added component input

--- client/src/pages/LoginUser.jsx
+++ client/src/pages/LoginUser.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Input from "../components/Input";
 
 const LoginUser = () => {
   const [formData, setFormData] = useState({
@@ -71,23 +72,5 @@ const LoginUser = () => {
     </div>
   );
 };
-
-const Input = ({ label, name, type = "text", value, onChange, hint }) => (
-  <div>
-    <label htmlFor={name} className="block text-sm font-medium text-gray-700">
-      {label}
-    </label>
-    <input
-      id={name}
-      name={name}
-      type={type}
-      value={value}
-      onChange={onChange}
-      required
-      className="mt-1 w-full px-3 py-2 border border-gray-300 focus:ring-2 focus:ring-bg-[#110C30] outline-none focus:border-bg-[#110C30] text-md"
-    />
-    {hint && <p className="text-xs text-gray-400 mt-1">{hint}</p>}
-  </div>
-);
 
 export default LoginUser;
--- client/src/pages/RegisterUser.jsx
+++ client/src/pages/RegisterUser.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import Input from "../components/Input";
+
 
 const RegisterUser = () => {
   const [formData, setFormData] = useState({
@@ -105,23 +107,4 @@ const RegisterUser = () => {
     </div>
   );
 };
-
-const Input = ({ label, name, type = "text", value, onChange, hint }) => (
-  <div>
-    <label htmlFor={name} className="block text-sm font-medium text-gray-700">
-      {label}
-    </label>
-    <input
-      id={name}
-      name={name}
-      type={type}
-      value={value}
-      onChange={onChange}
-      required
-      className="mt-1 w-full px-3 py-2 border border-gray-300 focus:ring-2 focus:ring-bg-[#110C30] outline-none focus:border-bg-[#110C30] text-md"
-    />
-    {hint && <p className="text-xs text-gray-400 mt-1">{hint}</p>}
-  </div>
-);
-
 export default RegisterUser;
