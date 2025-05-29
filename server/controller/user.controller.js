0b7208d2b0ec2e8558e7a0367af463106f47abfd
elisha <elishajameel2007@gmail.com>
2025-05-29 02:54:49 -0700

    completed user register controller

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,4 +1,19 @@
+import { userModel } from "../models/user.models.js";
+
 export const registerUser = async (req, res) => {
   const { name, email, password, phone, address, avatar } = req.body;
-  
+  try {
+    const createdUser = await userModel.create({
+      name,
+      email,
+      password,
+      phone,
+      address,
+      avatar,
+    });
+    res.status(201).json({ message: createdUser });
+  } catch (error) {
+    res.status(500).json({ message: error });
+    console.error("Error from User Register Controller: ", error);
+  }
 };
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
+import { registerUser } from "../controller/user.controller.js";
 
 const router = Router()
 
-router.get("/register", )
+router.get("/register", registerUser)
