11509dac8c130b35981bf209ce39bedfe5aeac89
elisha <elishajameel2007@gmail.com>
2025-05-29 02:59:34 -0700

    added db connection

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.routes.js";
+import { connectDb } from "./config/DB_CONNECTION.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
+connectDb();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(userRouter);
