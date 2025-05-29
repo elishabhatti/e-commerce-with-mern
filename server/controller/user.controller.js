d46a863e5f0d72095c062caa3931f6d1fcff81fd
elisha <elishajameel2007@gmail.com>
2025-05-29 03:10:00 -0700

    added validation in register user

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -2,6 +2,11 @@ import { userModel } from "../models/user.models.js";
 
 export const registerUser = async (req, res) => {
   const { name, email, password, phone, address, avatar } = req.body;
+
+  if (!name || !email || !password || !phone || !address || !avatar) {
+    return res.status(401).json({ message: "All field are required" });
+  }
+
   try {
     const createdUser = await userModel.create({
       name,
@@ -12,6 +17,7 @@ export const registerUser = async (req, res) => {
       avatar,
     });
     res.status(201).json({ message: createdUser });
+    
   } catch (error) {
     res.status(500).json({ message: error });
     console.error("Error from User Register Controller: ", error);
