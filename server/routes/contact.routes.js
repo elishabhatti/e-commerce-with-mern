1a341f98c4a8db9e12ffe59d9c30d14b00b651da
elisha <elishajameel2007@gmail.com>
2025-05-30 03:44:46 -0700

    added contact controller ref in contact routes

--- server/routes/contact.routes.js
+++ server/routes/contact.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { contactUs } from "../controller/contact.controller.js";
 
 const router = Router();
 
-router.post("/add-contact");
+router.post("/add-contact", contactUs);
 
 export default router;
