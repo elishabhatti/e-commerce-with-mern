eb25826e8adfaea900cdedcf82e78cc2b809a306
elisha <elishajameel2007@gmail.com>
2025-05-30 03:57:39 -0700

    changed the space between nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <>
       <header>
-        <div className="flex mb-20 justify-between border-b border-gray-300 items-center py-5 px-10">
+        <div className="flex mb-20 justify-between border-b border-gray-300 items-center py-3 px-10">
           <div className="text-2xl">
             <NavLink to="/">DEVIAS</NavLink>
           </div>
