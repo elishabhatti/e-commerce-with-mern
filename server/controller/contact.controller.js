457bc19b8b2cf9b4c5ce87a05e3b9d8ee94ada41
elisha <elishajameel2007@gmail.com>
2025-05-30 03:42:04 -0700

    added contact us controller

---
+++ server/controller/contact.controller.js
@@ -0,0 +1,17 @@
+import { contactModel } from "../models/contact.model";
+
+export const contactUs = async (req, res) => {
+  const { name, email, message } = req.body;
+
+  try {
+    if (!name || !email || !message) {
+      return res.status(404).json({ message: "All field are required" });
+    }
+
+    const createdContact = await contactModel.create({ name, email, message });
+    res.status(200).json({ message: "Contact Added", createdContact });
+  } catch (error) {
+    console.error("Error from contact us controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
