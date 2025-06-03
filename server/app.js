bbca08fe73bde9ad5773ffdd8beea2f86cd30171
elisha <elishajameel2007@gmail.com>
2025-06-03 04:54:00 -0700

    enable cors

--- server/app.js
+++ server/app.js
@@ -10,6 +10,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 connectDb();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
