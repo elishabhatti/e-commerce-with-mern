f38069b6e21a971700495db45c4d3fcdb36ef086
elisha <elishajameel2007@gmail.com>
2025-06-03 04:48:45 -0700

    added controller to get products

--- server/controller/product.controller.js
+++ server/controller/product.controller.js
@@ -1,2 +1,12 @@
 import { productModel } from "../models/product.models.js";
 
+export const getAllProductsData = async (req, res) => {
+  try {
+    const products = await productModel.find();
+    if (!products)
+      return res.status(500).json({ message: "Product Not Found Try Later" });
+    return res.status(200).json({ message: products });
+  } catch (error) {
+    console.error("Error From Get Products Controller: ", error);
+  }
+};
