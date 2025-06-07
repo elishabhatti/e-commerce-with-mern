c53febd868913d7e0554a9e806a6f123d2f44df6
elisha <elishajameel2007@gmail.com>
2025-06-07 14:21:59 +0500

    added route for product details page

--- server/routes/products.routes.js
+++ server/routes/products.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { getAllProductsData } from "../controller/product.controller.js";
+import {
+  getAllProductsData,
+  productDetails,
+} from "../controller/product.controller.js";
 
 const router = Router();
 
 router.get("/get-product", getAllProductsData);
+router.get("/products/:id", productDetails);
 
 export default router;
