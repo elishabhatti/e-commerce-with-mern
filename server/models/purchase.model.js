8cb7379d6652ed1003a35f78dadbb5257389477e
elisha <elishajameel2007@gmail.com>
2025-06-07 15:16:38 +0500

    added user id in purchase and contact schema

--- server/models/purchase.model.js
+++ server/models/purchase.model.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 const purchaseSchema = new mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     product: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Product",
