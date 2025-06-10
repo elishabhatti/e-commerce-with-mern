4a51fe76eeca5b0774dcf63016460894498499e7
elisha <elishajameel2007@gmail.com>
2025-06-10 16:52:34 +0500

    added verify authentication when add to cart

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ import userRouter from "./routes/user.routes.js";
 import contactRouter from "./routes/contact.routes.js";
 import productRouter from "./routes/products.routes.js";
 import purchaseRouter from "./routes/purchase.route.js";
+import cartRouter from "./routes/cart.route.js";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDb } from "./config/DB_CONNECTION.js";
@@ -35,5 +36,6 @@ app.use("/api/users", userRouter);
 app.use("/api/contact", verifyAuthentication, contactRouter);
 app.use("/api/products", verifyAuthentication, productRouter);
 app.use("/api/purchase", verifyAuthentication, purchaseRouter);
+app.use("/api/cart", verifyAuthentication, cartRouter);
 
 app.listen(PORT);
