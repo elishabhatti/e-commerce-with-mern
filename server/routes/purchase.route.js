a9c26e2b7939aa574c2874b94183654c111d12c0
elisha <elishajameel2007@gmail.com>
2025-06-11 13:00:09 +0500

    added route page and controller for get update purchase page for

--- server/routes/purchase.route.js
+++ server/routes/purchase.route.js
@@ -3,12 +3,15 @@ import {
   createPurchase,
   getPurchaseProduct,
   removePurchaseProduct,
+  getPurchaseProductById,
 } from "../controller/purchase.controller.js";
 
 const router = Router();
 
 router.post("/purchase-product", createPurchase);
 router.get("/get-purchase-product", getPurchaseProduct);
+router.get("/get-purchase-product/:id", getPurchaseProductById);
+router.post("/remove-purchased-product/:id", removePurchaseProduct);
 router.post("/remove-purchased-product/:id", removePurchaseProduct);
 
 export default router;
