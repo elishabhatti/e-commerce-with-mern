68bf491cf4d9370ceb342bc4b2178f533365848a
elisha <elishajameel2007@gmail.com>
2025-06-12 18:31:31 +0500

    make the footer to the bottom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,33 +14,35 @@ import Cart from "./pages/Cart";
 import ProductDetails from "./pages/ProductDetails";
 import PurchaseProducts from "./pages/PurchaseProducts";
 import CartDetails from "./pages/CartDetails";
-import EditProfile from "./pages/EditProfile"
+import EditProfile from "./pages/EditProfile";
 import UpdateCartProduct from "./pages/UpdateCartProduct";
 import UpdatePurchaseProduct from "./pages/UpdatePurchaseProduct";
 
 const App = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route path="/register" element={<RegisterUser />} />
-          <Route path="/login" element={<LoginUser />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/service" element={<Services />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/purchase" element={<PurchaseProducts />} />
-          <Route path="/logout" element={<LogoutPage />} />
-          <Route path="/product-details/:id" element={<ProductDetails />} />
-          <Route path="/product-cart/:id" element={<CartDetails />} />
-          <Route path="/update-cart-product/:id" element={<UpdateCartProduct />} />
-          <Route path="/update-purchase/:id" element={<UpdatePurchaseProduct />} />
-          <Route path="/edit-profile/:id" element={<EditProfile />} />
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<Page404 />} />
-        </Routes>
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/register" element={<RegisterUser />} />
+            <Route path="/login" element={<LoginUser />} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/service" element={<Services />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/purchase" element={<PurchaseProducts />} />
+            <Route path="/logout" element={<LogoutPage />} />
+            <Route path="/product-details/:id" element={<ProductDetails />} />
+            <Route path="/product-cart/:id" element={<CartDetails />} />
+            <Route path="/update-cart-product/:id" element={<UpdateCartProduct />} />
+            <Route path="/update-purchase/:id" element={<UpdatePurchaseProduct />} />
+            <Route path="/edit-profile/:id" element={<EditProfile />} />
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Page404 />} />
+          </Routes>
+        </main>
         <Footer />
       </BrowserRouter>
     </div>
