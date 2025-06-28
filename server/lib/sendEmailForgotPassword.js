03c9293fd8712175a665c759f9de68b0d0cb04af
elisha <elishajameel2007@gmail.com>
2025-06-28 14:17:48 +0500

    added send email function with resend

---
+++ server/lib/sendEmailForgotPassword.js
@@ -0,0 +1,24 @@
+import { Resend } from "resend";
+import dotenv from "dotenv";
+dotenv.config();
+
+const resend = new Resend(process.env.FORGOT_PASSWORD_API_KEY);
+
+export const sendEmail = async ({ to, subject, html }) => {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "E-Commerce App | MERN Series <website@resend.dev>",
+      to: [to],
+      subject,
+      html,
+    });
+
+    if (error) {
+      return console.error("Error", error);
+    } else {
+      console.log(data);
+    }
+  } catch (error) {
+    console.error("Try Catch Error", error);
+  }
+};
