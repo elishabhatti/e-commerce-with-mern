7073f14019feeb099b8effafcbe026ab0d8c1948
elisha <elishajameel2007@gmail.com>
2025-06-29 15:39:00 +0500

    added on change in reset password for inputs

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -5,10 +5,21 @@ import { useSearchParams } from "react-router-dom";
 const ResetPassword = () => {
   const [searchParams] = useSearchParams();
   const token = searchParams.get("token");
-
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
   const [message, setMessage] = useState("");
+
+  const [resetPassword, setResetPassword] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setResetPassword((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
   const handleReset = async (e) => {
     e.preventDefault();
@@ -40,19 +51,31 @@ const ResetPassword = () => {
         <h2 className="text-2xl font-bold mb-4 text-center">Reset Password</h2>
 
         <input
-          type="password"
-          placeholder="New password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={resetPassword.email}
+          onChange={handleChange}
           required
           className="w-full p-2 border border-gray-300 rounded mb-4"
         />
 
         <input
           type="password"
+          name="password"
+          placeholder="New password"
+          value={resetPassword.password}
+          onChange={handleChange}
+          required
+          className="w-full p-2 border border-gray-300 rounded mb-4"
+        />
+
+        <input
+          type="password"
+          name="confirmPassword"
           placeholder="Confirm new password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
+          value={resetPassword.confirmPassword}
+          onChange={handleChange}
           required
           className="w-full p-2 border border-gray-300 rounded mb-4"
         />
