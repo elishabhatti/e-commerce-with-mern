4863195bde0b7dfe36246bad135184dd3b8c7b32
elisha <elishajameel2007@gmail.com>
2025-07-09 12:07:10 +0500

    added route for upload profile photo

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -12,11 +12,13 @@ import {
   verifyEmailWithCode,
 } from "../controller/user.controller.js";
 import { verifyAuthentication } from "../middlewares/verifyAuthentication.js";
+import { upload } from "../middlewares/upload.js";
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.get("/profile", verifyAuthentication, getProfileData);
+router.post("/profile/upload-photo", upload.single("photo"), updateProfilePhoto);
 router.post("/verify-email", verifyAuthentication, verifyEmail);
 router.post("/verify-email-code", verifyAuthentication, verifyEmailWithCode);
 router.post("/login", loginUser);
