6423581483de836ab3961bc73900430b39cf7148
elisha <elishajameel2007@gmail.com>
2025-07-10 11:49:53 +0500

    added the logic for getting the wishlist products

--- client/src/pages/Wishlist.jsx
+++ client/src/pages/Wishlist.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const Wishlist = () => {
-  return (
-    <div>Wishlist</div>
-  )
-}
+  const [isLoading, setIsLoading] = useState(true);
+  async function getAllWishListProducts() {
+    try {
+      const res = await axios.get(
+        "http://localhost:3000/api/users/get-wishlist-product",
+        {
+          withCredentials: true,
+        }
+      );
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to load products.");
+    } finally {
+      setIsLoading(false); // ✅ Stop loading regardless of success/failure
+    }
+  }
+  if (isLoading) return <LoadingSpinner />;
+  return <div>Wishlist</div>;
+};
 
-export default Wishlist
+export default Wishlist;
