75394d11af1f2f072ecb055358a93d4d9132749a
elisha <elishajameel2007@gmail.com>
2025-07-23 12:51:24 +0500

    added helper get and post  request in api.js

---
+++ client/utils/api.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const token = localStorage.getItem("token");
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:3000/api",
+  withCredentials: true,
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+});
+
+// Generic GET
+export const getRequest = async (url) => {
+  const res = await axiosInstance.get(url);
+  return res.data.data;
+};
+
+// Generic POST
+export const postRequest = async (url, body = {}) => {
+  const res = await axiosInstance.post(url, body);
+  return res.data.data;
+};
+
+export default axiosInstance;
